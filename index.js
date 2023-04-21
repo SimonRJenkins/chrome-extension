866c231e1ed25327f94d25752e911f50d73bf901
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-04-21 07:56:26 +0100

    Added list items by using innerHTML

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function () {
-    //myLeads.push(inputEl.value)
-    //console.log(myLeads)
-
-    for (i = 0; i < myLeads.length; i++) {
-        console.log(myLeads[i])
-    }
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
+let listItems = ""
+for (i = 0; i < myLeads.length; i++) {
+
+    listItems += "<li>" + myLeads[i] + "</li>"
+
+}
+
+ulEl.innerHTML = listItems
