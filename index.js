11e41476cac5c408012cdb512736a5529fde66c1
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-05-03 07:02:33 +0100

    Changed localStorage to work correctly when typing in a lead and saving it

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let myLeads = `["www.awesomelead.com"]`
-
-myLeads = JSON.parse(myLeads)
-myLeads.push("www.lead2.com")
-myLeads = JSON.stringify(myLeads)
-console.log(typeof myLeads)
-
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
@@ -20,11 +20,12 @@ function renderLeads() {
     let listItems = ""
     for (i = 0; i < myLeads.length; i++) {
         listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
-            </a>
-        </li>`
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
